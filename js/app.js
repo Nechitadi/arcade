795eda038435957550ab8101a39d3447249cfb85
JP Lacy <lacyjpr@gmail.com>
2015-10-09 00:20:57 -0700

    test: Test without use strict

--- js/app.js
+++ js/app.js
@@ -89,7 +89,7 @@ var playerLives = 5;
 
 // Required method for game
 Player.prototype.update = function() {
-    "use strict";
+    "use strict"
     // document.getElementById("lives").innerHTML = "Lives: " + playerLives;
     // document.getElementById("score").innerHTML = "Score: " + playerScore;
     // document.getElementById("difficulty").innerHTML = "Difficulty: " + speedMultiplier;
