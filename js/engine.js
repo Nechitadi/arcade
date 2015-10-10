188dd1b86f19279cc8caffd69ffb7ae56868fcee
JP Lacy <lacyjpr@gmail.com>
2015-10-09 19:33:39 -0700

    feat: Add game over game state

--- js/engine.js
+++ js/engine.js
@@ -66,7 +66,7 @@ var Engine = (function(global) {
      * game loop.
      */
     function init() {
-        reset();
+        //reset();
         lastTime = Date.now();
         main();
     }
@@ -81,6 +81,7 @@ var Engine = (function(global) {
      * on the entities themselves within your app.js file).
      */
     function update(dt) {
+        console.log ("update " + currentGameState);
         switch (currentGameState) {
             case "startGame":
                 document.addEventListener('keyup', function(e) {
@@ -100,9 +101,17 @@ var Engine = (function(global) {
                 ctx.clearRect(0,0,canvas.width,canvas.height);
                 break;
             case "gameOver":
+                document.addEventListener('keyup', function(e) {
+                "use strict";
+                var allowedKeys = {
+                    13: 'enter'
+                };
+                if (allowedKeys = 'enter') {
+                    currentGameState = "inGame";
+                    }
+                });
                 break;
         }
-
     }
 
     /* This is called by the update function  and loops through all of the
@@ -169,7 +178,6 @@ var Engine = (function(global) {
             }
             break;
         case "inGame":
-            document.getElementById("start").innerHTML = "";
             /* This array holds the relative URL to the image used
              * for that particular row of the game level.
              */
@@ -204,6 +212,38 @@ var Engine = (function(global) {
             renderEntities();
             break;
         case "gameOver":
+            var rowImages = [
+                    'images/water-block.png',   // Top row is water
+                    'images/stone-block.png',   // Row 1 of 3 of stone
+                    'images/stone-block.png',   // Row 2 of 3 of stone
+                    'images/stone-block.png',   // Row 3 of 3 of stone
+                    'images/grass-block.png',   // Row 1 of 2 of grass
+                    'images/grass-block.png'    // Row 2 of 2 of grass
+                ],
+                numRows = 6,
+                numCols = 5,
+                row, col;
+
+            /* Loop through the number of rows and columns we've defined above
+             * and, using the rowImages array, draw the correct image for that
+             * portion of the "grid"
+             */
+            for (row = 0; row < numRows; row++) {
+                for (col = 0; col < numCols; col++) {
+                    /* The drawImage function of the canvas' context element
+                     * requires 3 parameters: the image to draw, the x coordinate
+                     * to start drawing and the y coordinate to start drawing.
+                     * We're using our Resources helpers to refer to our images
+                     * so that we get the benefits of caching these images, since
+                     * we're using them over and over.
+                     */
+                    ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+                    ctx.fillStyle = "blue"
+                    ctx.font = "20px sans-serif"
+                    ctx.fillText("Press Enter To Restart", 155, 175);
+                    ctx.fillStyle = "blue"
+                }
+            }
             break;
             }
 
@@ -230,10 +270,11 @@ var Engine = (function(global) {
      * those sorts of things. It's only called once by the init() method.
      */
     function reset() {
+        currentGameState = "gameOver";
         player = new Player;
         speedMultiplier = 40;
         playerScore = 0;
-        playerLives = 5;
+        playerLives = 5; // Necessary for game to restart properly
         allEnemies = [];
         //Instantiate all enemies, set to 3
         for (var i = 0; i < 3; i++) {
@@ -263,6 +304,8 @@ var Engine = (function(global) {
     global.ctx = ctx;
     global.main = main;
     global.reset = reset;
+    global.update = update;
+    global.render = render;
     global.currentGameState = currentGameState;
 })(this);
 
